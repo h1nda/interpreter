3654a905b9b724f509876ae0160db1761dcbb077
Hinda <hinda2sani@gmailc.om>
2021-01-02 20:39:36 +0200

    infix to postfix

--- interpreter.cpp
+++ interpreter.cpp
@@ -41,77 +41,94 @@ using namespace std;
 //};
 
 bool Precedence(char op) {
-    if (op == '*' || op == '/')
-        return 1;
-    return 0;
+	if (op == '*' || op == '/')
+		return 1;
+	return 0;
 }
 
 bool isDigit(char c) {
-    return c >= '0' && c <= '9';
+	return c >= '0' && c <= '9';
 }
 int build_int(string s, size_t fidx, size_t* lidx) {
-    int result = 0;
-    for (size_t i = fidx; i < s.length(); i++) {
-        if (isDigit(s[i])) {
-            result = result * 10 + (s[i] - '0');
-        }
-        else
-        {
-            *lidx = i;
-            return result;
+	int result = 0;
+	for (size_t i = fidx; i < s.length(); i++) {
+		if (isDigit(s[i])) {
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			*lidx = i;
+			return result;
 
-        }
+		}
 
-    }
+	}
 }
 ////Dijkstra's Shunting Algorithm explanation
 void ShuntingAlgorithm_Test(string expr) {
-    //    /*Algorithm: whenever we encounter a digit, we run it through build_int to check if it's a multidigit number, 
-    //    then we push it onto the queue,
-    //    whenever we encounter an operation (*,/,+,-, ()) we push it into the stack
-    //    if the top element in the stack has higher precedence than the current one to be pushed, we pop it and enqueue
-    //    otherwise we push the current.*/
-    stack<char> st; // push 
-    //string infix;
-    string infix;
-    for (int i = 0; i < expr.length(); i++) {
-   
-            while (isDigit(expr[i])) {
-                infix.push_back(expr[i]);
-                i++;
-            }
+	//    /*Algorithm: whenever we encounter a digit, we run it through build_int to check if it's a multidigit number, 
+	//    then we push it onto the queue,
+	//    whenever we encounter an operation (*,/,+,-, ()) we push it into the stack
+	//    if the top element in the stack has higher precedence than the current one to be pushed, we pop it and enqueue
+	//    otherwise we push the current.*/
+	stack<char> st; // push 
+	//string infix;
+	string infix;
+	for (int i = 0; i < expr.length(); i++) {
 
-         if (expr[i] == '*' || expr[i] == '/' || expr[i] == '+' || expr[i] == '-') {
-            if (st.empty() || Precedence(st.top()) < Precedence(expr[i])) {
-                st.push(expr[i]);
-            }
-            else {
-                infix.push_back(' ');
-                infix.push_back(st.top());
-                st.pop();
-                st.push(expr[i]);
-            }
-        }
-        infix.push_back(' ');
-    }
-    while (!st.empty()) {
-        infix.push_back(st.top());
-        st.pop();
-        infix.push_back(' ');
-    }
+		while (isDigit(expr[i])) {
+			infix.push_back(expr[i]);
+			i++;
+		}
+		if (expr[i] == '(') {
+			st.push(expr[i]);
+			continue;
+		}
+		else if (expr[i] == ')') {
+			while (!st.empty()) {
+				if (st.top() == '(') {
+					
+					st.pop();
+					break;
+				}
+				else {
+					infix.push_back(' ');
+					infix.push_back(st.top());
+					st.pop();
+				}
+			}
+		}
+		else if (expr[i] == '*' || expr[i] == '/' || expr[i] == '+' || expr[i] == '-') {
+			if (st.empty() || st.top() == '(' || Precedence(st.top()) < Precedence(expr[i])) {
+				st.push(expr[i]);
+			}
+			else {
+				infix.push_back(' ');
+				infix.push_back(st.top());
+				st.pop();
+				st.push(expr[i]);
+			}
+		}
+		infix.push_back(' ');
+	}
+	while (!st.empty()) {
+		infix.push_back(st.top());
+		st.pop();
+		infix.push_back(' ');
+	}
 
-  
-//    /*Using a bool as a BODMAS rule we determine precedence:
-//    HIGH (1) are * and /;
-//    LOW (0) are - and +;
-//    If they are of the same precedence we calculate using left associativity.*/
-    cout << infix;
+
+	//    /*Using a bool as a BODMAS rule we determine precedence:
+	//    HIGH (1) are * and /;
+	//    LOW (0) are - and +;
+	//    If they are of the same precedence we calculate using left associativity.*/
+	cout << infix;
 }
 
 int main()
 {
-    string test = "33*202+26";
-    ShuntingAlgorithm_Test(test);
+	string test = "90/5-10*11";
+	ShuntingAlgorithm_Test(test);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
